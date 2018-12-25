2a77e64abbd751a65221d2ee0c8dff45dc3f1695
Martin <martinwang7963@gmail.com>
2018-12-25 10:32:45 +0800

    Change Project structure

---
+++ LeecodeSolution/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using SolutionLib;
+
+namespace LeecodeSolution {
+    class Program {
+        static void Main (string[] args) {
+            Question1 q = new Question1 ();
+            q.Call();
+        }
+    }
+}
---
+++ SolutionLib/Question1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SolutionLib {
+    public class Question1 {
+        public void TwoSum () {
+
+        }
+    }
+}
