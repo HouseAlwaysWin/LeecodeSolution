db7d816c4c3959849a06a5ab070a6c56f8aaa104
Martin <martinwang7963@gmail.com>
2018-12-25 17:24:58 +0800

    Set vscode setting

--- LeecodeSolution/Program.cs
+++ LeecodeSolution/Program.cs
@@ -7,7 +7,7 @@ namespace LeecodeSolution {
     class Program {
         static void Main (string[] args) {
             int[] nums = { 1, 2, 3, 4 };
-            int target = 5;
+            int target = 7;
             Question1 q1 = new Question1 ();
             WatchDog.ShowPerformance(q1.TwoSum, nums, target);
         }
