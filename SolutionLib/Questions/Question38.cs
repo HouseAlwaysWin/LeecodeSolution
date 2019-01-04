865056fb31495a9d6d8475064d4339bed6cef982
Martin <martinwang7963@gmail.com>
2019-01-04 14:54:08 +0800

    Add new question38

---
+++ SolutionLib/Questions/Question38.cs
@@ -0,0 +1,44 @@
+using SolutionLib.Tools;
+
+namespace SolutionLib.Questions {
+    public class Question38 : IQuestion {
+        /*
+        The count-and-say sequence is the sequence of integers with the first five terms as following:
+
+        1.     1
+        2.     11
+        3.     21
+        4.     1211
+        5.     111221
+        1 is read off as "one 1" or 11.
+        11 is read off as "two 1s" or 21.
+        21 is read off as "one 2, then one 1" or 1211.
+
+        Given an integer n where 1 ≤ n ≤ 30, generate the nth term of the count-and-say sequence.
+
+        Note: Each term of the sequence of integers will be represented as a string.
+
+        
+
+        Example 1:
+
+        Input: 1
+        Output: "1"
+        Example 2:
+
+        Input: 4
+        Output: "1211"
+         */
+        public void Run () {
+            int n = 100;
+            System.Console.WriteLine ("V1:");
+            WatchDog.ShowPerformance (CountAndSay, 100);
+        }
+
+        public string CountAndSay (int n) {
+
+            return string.Empty;
+        }
+
+    }
+}
