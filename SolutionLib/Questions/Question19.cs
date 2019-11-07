b750b0ec49951e978488a24ba3b8a95018a7a7c8
Martin <martinwang7963@gmail.com>
2019-11-07 09:25:04 +0800

    Add question19

--- SolutionLib/Questions/Question19.cs
+++ SolutionLib/Questions/Question19.cs
@@ -1,5 +1,8 @@
 using System;
 using System.Net.NetworkInformation;
+using System.Security.Principal;
+using SolutionLib.Tools;
+
 namespace SolutionLib.Questions {
     public class Question19 : IQuestion {
         /*
@@ -19,27 +22,46 @@ namespace SolutionLib.Questions {
         Could you do this in one pass?
          */
         public void Run () {
-            throw new System.NotImplementedException ();
+            ListNode test1 = new ListNode (1) {
+                next = new ListNode (2) {
+                next = new ListNode (3) {
+                next = new ListNode (4) {
+                next = new ListNode (5)
+                }
+                }
+                }
+            };
+
+            ListNode test2 = new ListNode (1) {
+                next = new ListNode (2)
+            };
+
+            WatchDog.ShowPerformance<ListNode, int, ListNode> (RemoveNthFromEnd, test1, 2);
+            WatchDog.ShowPerformance<ListNode, int, ListNode> (RemoveNthFromEnd, test2, 2);
         }
 
         public ListNode RemoveNthFromEnd (ListNode head, int n) {
             if (head == null) return null;
-            int len = 0;
+            int max = 1;
             var refs = head;
             while (refs.next != null) {
                 refs = refs.next;
-                len++;
+                max++;
             }
-            int index = len - n;
-            if (Math.Abs (index) > len) return null;
+
+            int index = max - n;
             int count = 0;
             var prev = head;
-            while (count != index) {
+            if (index == 0) {
+                head = head.next;
+                return head;
+            }
+            while (count < index - 1) {
                 prev = prev.next;
                 count++;
             }
 
-            if (prev != null && prev.next != null) {
+            if (prev.next != null) {
                 prev.next = prev.next.next;
             }
 
