1404ac7b90c18630944746420429f3b5e474f54e
Martin <martinwang7963@gmail.com>
2019-11-27 15:35:22 +0800

    Add Question209

---
+++ SolutionLib/Questions/Question209.cs
@@ -0,0 +1,23 @@
+namespace SolutionLib.Questions209 {
+
+    public class Question209 : IQuestion {
+        /*
+        Given an array of n positive integers and a positive integer s, 
+        find the minimal length of a contiguous subarray of which the sum ≥ s. If there isn't one, return 0 instead.
+
+        Example: 
+
+        Input: s = 7, nums = [2,3,1,2,4,3]
+        Output: 2
+        Explanation: the subarray [4,3] has the minimal length under the problem constraint.
+        Follow up:
+        If you have figured out the O(n) solution, try coding another solution of which the time complexity is O(n log n). 
+         */
+        public void Run () {
+            WatchDog
+        }
+        public int MinSubArrayLen (int s, int[] nums) {
+
+        }
+    }
+}
